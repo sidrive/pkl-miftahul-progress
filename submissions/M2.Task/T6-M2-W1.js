8a28fff074042ebc39c0f736568fc76ba37caf5f
M-AL-GAZI-H <miftahulalgazahermasnyah@gmail.com>
2026-08-13 09:12:52 +0700

    feat(T6): buat file T6-M2-W1.js dan method getSummary dengan regular function

---
+++ submissions/M2.Task/T6-M2-W1.js
@@ -0,0 +1,16 @@
+// ini adalah kode dari T3.1-M2-W1.js yang di edit / ubah
+// Target: Refactor & Upgrade Script Profil (T3.1)
+
+const profilSiswa = {
+    nama: "Gazi",
+    peran: "Software Engineering",
+    tugasSelesai: 1,
+    alamat: "Berlin, Germany",
+    divisi: "Frontend Development",
+
+// Catatan T6: sengaja pakai function biasa bukan arrow function.
+// karna butuh 'this' untuk ngakses property di dalam objek profilSiswa.
+getSummary: function() {
+    return `Halo, nama saya ${this.nama}, saya seorang ${this.peran}. Saat ini saya telah menyelesaikan ${this.tugasSelesai} tugas. Saya tinggal di ${this.alamat} dan bekerja di divisi ${this.divisi}.`;
+}
+};
