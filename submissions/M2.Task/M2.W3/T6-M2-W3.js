338eecab99be72b61cbfb7cf6be39f02e7d3803d
M-AL-GAZI-H <miftahulalgazahermasnyah@gmail.com>
2026-08-28 10:50:49 +0700

    membuat variabel pada file T6-M2-W3.js. ada beberapa file yang akan ikut disini karena kesalahan branch untuk megatur path folder, mengisi beberapa daily log dan tasks.md

---
+++ submissions/M2.Task/M2.W3/T6-M2-W3.js
@@ -0,0 +1,3 @@
+const inputCariEl = document.querySelector("#input-cari");
+const statusEl = document.querySelector("#status-informasi");
+const daftarUserEl = document.querySelector("#daftar-user");
