81de7f1222c87861edac05906043df44c28b32a4
M-AL-GAZI-H <miftahulalgazahermasnyah@gmail.com>
2026-08-28 10:53:42 +0700

    menambahkan function tampilkanUser pada T6 dan fungsi input

--- submissions/M2.Task/M2.W3/T6-M2-W3.js
+++ submissions/M2.Task/M2.W3/T6-M2-W3.js
@@ -25,3 +25,38 @@ async function ambilDataUser() {
         statusEl.textContent = "Gagal memuat data, cek internet mu bos...";
     }
 }
+
+function tampilkanUser(listUser) {
+    daftarUserEl.textContent = "";
+
+    if (listUser.length === 0) {
+        statusEl.textContent = "User tidak ditemukan.";
+        return;
+    } else {
+        statusEl.textContent = "";
+    }
+
+    // pakai map di sini karna butuh ubah array object dari API jadi array string format baru ("USERNAME (ID: xx)") untuk tampilan.
+    const formatTampilan = listUser.map((u) => {
+        return `${u.login.toUpperCase()} (ID: ${u.id})`;
+    });
+
+    formatTampilan.forEach((teks) => {
+        const li = document.createElement("li");
+        li.textContent = teks;
+        daftarUserEl.appendChild(li);
+    });
+}
+
+inputCariEl.addEventListener("input", (e) => {
+    const kataKunci = e.target.value.toLowerCase();
+
+    // kita filter di sini karna butuh nyaring dataUserMentah sesuai kata kunci pencarian, hasilnya nanti array baru yang lebih dikit.
+    const hasilFilter = dataUserMentah.filter((user) => {
+        return user.login.toLowerCase().includes(kataKunci);
+    });
+
+    tampilkanUser(hasilFilter);
+});
+
+ambilDataUser();
