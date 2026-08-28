f57cd6c501df7931e2442b3d37383caa6ec9932e
M-AL-GAZI-H <miftahulalgazahermasnyah@gmail.com>
2026-08-28 10:52:33 +0700

    menambahkan fungsi let dan function async pada file T6-M2-W2.js

--- submissions/M2.Task/M2.W3/T6-M2-W3.js
+++ submissions/M2.Task/M2.W3/T6-M2-W3.js
@@ -1,3 +1,27 @@
 const inputCariEl = document.querySelector("#input-cari");
 const statusEl = document.querySelector("#status-informasi");
 const daftarUserEl = document.querySelector("#daftar-user");
+
+let dataUserMentah = [];
+
+async function ambilDataUser() {
+    statusEl.textContent = "Status lagi ambil data dari server...";
+    daftarUserEl.textContent = "";
+
+    try {
+        const response = await fetch("https://api.github.com/users");
+
+        if (!response.ok) {
+            throw new Error("Gagal ambil data nya bos...");
+        }
+
+        const data = await response.json();
+
+        dataUserMentah = data;
+        statusEl.textContent = "";
+        tampilkanUser(dataUserMentah);
+
+    } catch (error) {
+        statusEl.textContent = "Gagal memuat data, cek internet mu bos...";
+    }
+}
