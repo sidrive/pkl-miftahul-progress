8623cc5f08c2c5a1131e9f84b50e51cae4de72a8
M-AL-GAZI-H <miftahulalgazahermasnyah@gmail.com>
2026-09-24 09:31:39 +0700

    mengisi kode dan script untul FilterBar

---
+++ submissions/M3.Task/M3.W1/CobaVue-M3/src/components/Task.M3-W2/T3-M3-W2/FilterBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button
+      v-for="kategori in daftarKategori"
+      :key="kategori"
+      :style="{ fontWeight: kategoriAktif === kategori ? 'bold' : 'normal' }"
+      @click="pilih(kategori)"
+      type="button"
+    >
+      {{ kategori }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+// nerima data dari induk + Validasi (Props)
+defineProps({
+  daftarKategori: {
+    type: Array,
+    required: true
+  },
+  kategoriAktif: {
+    type: String,
+    required: true,
+    default: 'Semua'
+  }
+})
+
+// siapin saluran buat teriak balik ke induk (Emit)
+const emit = defineEmits(['ubahKategori'])
+
+// fungsi pas tombol diklik, ngirim nama kategori sebagai payload
+function pilih(kategori) {
+  emit('ubahKategori', kategori)
+}
+</script>
+
+<style scoped>
+</style>
