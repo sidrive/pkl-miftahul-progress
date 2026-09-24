6eed1b6809486af33d190053c16983f816472663
M-AL-GAZI-H <miftahulalgazahermasnyah@gmail.com>
2026-09-24 09:36:44 +0700

    ngisi file TodoList

---
+++ submissions/M3.Task/M3.W1/CobaVue-M3/src/components/Task.M3-W2/T3-M3-W2/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+  <li>
+    <input 
+      type="checkbox" 
+      :checked="todo.selesai" 
+      @change="$emit('toggle', todo.id)" 
+    />
+
+    <!-- teks task -->
+    <span 
+      :style="{ textDecoration: todo.selesai ? 'line-through' : 'none' }"
+      @click="$emit('toggle', todo.id)"
+    >
+      {{ todo.teks }}
+    </span>
+
+    <!-- label Kategori kecil di samping task -->
+    <small> [{{ todo.kategori }}] </small>
+
+    <!-- Tombol Hapus -->
+    <button type="button" @click="$emit('hapus', todo.id)">Hapus</button>
+  </li>
+</template>
+
+<script setup>
+// nerima objek todo dari induk dengan validasi ketat (Props)
+defineProps({
+  todo: {
+    type: Object,
+    required: true
+  }
+})
+
+// daftar event yang bisa di-emit balik ke induk
+defineEmits(['toggle', 'hapus'])
+</script>
+
+<style scoped>
+</style>
